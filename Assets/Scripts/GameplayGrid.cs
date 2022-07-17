7218b6e8254a748cb8889c22ab5ee8ac57740580
Kyle Grenier <kyle.grenier11@gmail.com>
2022-07-17 01:15:00 -0500

    Fixed rows and columns being switched
    Refactored code to be more readable and make more sense:

--- Assets/Scripts/GameplayGrid.cs
+++ Assets/Scripts/GameplayGrid.cs
@@ -4,77 +4,88 @@ using UnityEngine;
 
 namespace KpattGames.Tools
 {
-
-    /// <summary>
-    /// Represents a node in a grid.
-    /// </summary>
-    public readonly struct Node
-    {
-        public GameObject Obj { get; }
-        public Vector3 Position => Obj.transform.position;
-
-        public Node(GameObject obj)
-        {
-            Obj = obj;
-        }
-    }
-    
     /// <summary>
     /// Represents the grid the game takes place on.
     /// </summary>
     public class GameplayGrid : MonoBehaviour
     {
-        [SerializeField] private int rows;
-        [SerializeField] private int columns;
+        [SerializeField][Min(0)] private int rows;
+        [SerializeField][Min(0)] private int columns;
 
         [SerializeField] private GameObject pointPrefab;
 
         public Node[,] Nodes { get; private set; }
+        private Transform nodeParent;
 
         
         
         private void Awake()
         {
             Nodes = new Node[rows, columns];
+            nodeParent = transform.GetChild(0);
         }
 
         private void Start()
+        {
+            CreateGrid();
+        }
+
+        [ContextMenu("Reset Grid")]
+        public void ResetGrid()
+        {
+            // Re-initialize Nodes in case we modified rows and cols.
+            Nodes = new Node[rows, columns];
+            
+            // We have to loop through the nodeParent
+            // because we may have changed the rows and/or columns
+            // fields.
+            int childCount = nodeParent.childCount;
+            for (int i = 0; i < childCount; ++i)
+            {
+                Destroy(nodeParent.GetChild(i).gameObject);
+            }
+
+            nodeParent.localPosition = new Vector3(0.5f, -0.5f);
+            CreateGrid();
+        }
+        
+        private void CreateGrid()
         {
             Vector3 localScale = transform.localScale;
             float width = localScale.x;
             float height = localScale.y;
 
-            float rowWidth = height / rows;
-            float columnWidth = width / columns;
-
-            float prevX = 0;
+            float xOffset = width / columns;
+            float yOffset = height / rows;
+            
+            
+            
+            int rowIndex = 0;
             float prevY = 0;
 
-            int rowIndex = 0;
-            int columnIndex = 0;
-            
-            for (float row = rowWidth; row <= rowWidth * rows; row += rowWidth)
+            // Adding the nodes across each row.
+            for (float yPos = yOffset; yPos <= yOffset * rows; yPos += yOffset)
             {
-                for (float col = columnWidth; col <= columnWidth * columns; col += columnWidth)
+                float prevX = 0;
+                int colIndex = 0;
+                
+                for (float xPos = xOffset; xPos <= xOffset * columns; xPos += xOffset)
                 {
-                    var pos = new Vector2((row + prevX) / 2, -(col + prevY) / 2);
+                    Vector2 pos = new Vector2((xPos + prevX) / 2, -(yPos + prevY) / 2);
 
                     var clone = Instantiate(pointPrefab, pos, Quaternion.identity);
-                    clone.transform.parent = transform.GetChild(0);
+                    clone.transform.parent = nodeParent;
                     
-                    Nodes[rowIndex, columnIndex] = new Node(clone);
+                    Nodes[rowIndex, colIndex++] = new Node(clone);
                     
-                    ++columnIndex;
-                    prevY = col;
+                    prevX = xPos;
                 }
 
-                columnIndex = 0;
-                prevX = row;
-                prevY = 0;
+                prevY = yPos;
                 ++rowIndex;
             }
 
-            transform.GetChild(0).localPosition = Vector2.zero;
+            nodeParent.localPosition = Vector3.zero;
         }
     }   
 }
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace KpattGames.Tools
+{
+    /// <summary>
+    /// Represents a node in a grid.
+    /// </summary>
+    public readonly struct Node
+    {
+        public GameObject Obj { get; }
+        public Vector3 Position => Obj.transform.position;
+
+        public Node(GameObject obj)
+        {
+            Obj = obj;
+        }
+    }
+}
